358b87e7e19ec4c7e93bc76c7fc591749da0ebe8
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-04-24 22:01:31 -0400

    Start nats-consumer service, move all services to run on the host network driver, update nats-producer with experimental code.

--- nats-producer/app/src/index.ts
+++ nats-producer/app/src/index.ts
@@ -1,1 +1,10 @@
-const NATS = require("nats");
+import * as NATS from "nats";
+import * as process from "process";
+
+const client = NATS.connect(`nats://${process.env["NATS_HOST"]}:${process.env["NATS_PORT"]}`);
+
+for (let i = 0; i < 10; i++) {
+  client.publish("foo", `Hello, world #${i}!`);
+}
+
+client.close();
