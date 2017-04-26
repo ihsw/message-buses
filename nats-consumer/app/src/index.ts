9a5b272304f7a5dd760aa1c50f6c44d0bf8ebaf9
Adrian Parker <adrian.parker@smith.co>
2017-04-26 10:12:11 -0400

    Refactor express app route mounting into its own package.

--- nats-consumer/app/src/index.ts
+++ nats-consumer/app/src/index.ts
@@ -1,7 +1,6 @@
 import * as process from "process";
 import * as NATS from "nats";
-import * as express from "express";
-import * as HttpStatus from "http-status";
+import getApp from "./app";
 
 // parsing env vars
 const natsHost = process.env["NATS_HOST"];
@@ -12,120 +11,11 @@ const appPort = Number(process.env["APP_PORT"]);
 const client = NATS.connect(`nats://${natsHost}:${natsPort}`);
 console.log(`Connected to NATS server ${natsHost}:${natsPort}`);
 
-// setting up an express app
-const app = express();
-
-// setting up routes
-app.get("/timeout", (_, res) => {
-  const sId = client.subscribe("invalid-queue", () => { return; });
-  client.timeout(sId, 5 * 1000, 0, (timeoutSid) => {
-    res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-      .send(`Timed out with sid ${timeoutSid}!`);
-  });
-});
-
-app.get("/subscribers", (_, res) => res.send(client.numSubscriptions()));
-
-app.get("/:queue", (req, res) => {
-  const queue = req.params.queue;
-
-  client.publish("queues", queue);
-
-  const sId = client.subscribe(queue, (msg) => {
-    res.send(`received ${msg} on queue ${queue}`);
-    client.unsubscribe(sId);
-  });
-});
-
-app.get("/:queue/count/:count", (req, res) => {
-  res.setHeader("content-type", "text/plain");
-
-  // parsing params
-  const queue = req.params.queue;
-  const count = Number(req.params.count);
-
-  // flagging a new queue to have X messages published
-  client.publish("queueWaiting", JSON.stringify({ count: count, queue: queue }));
-
-  // setting up a full request timeout
-  const timeout = 5 * 1000;
-  const tId = setTimeout(() => {
-    if (res.headersSent) {
-      return;
-    }
-
-    res.send("Request timeout!");
-  }, timeout);
-
-  // starting up a subscriber waiting for messages
-  let messageCount = 0;
-  const sId = client.subscribe(queue, (msg) => {
-    res.write(`${msg}\n`);
-
-    if (++messageCount === count) {
-      client.unsubscribe(sId);
-      clearTimeout(tId);
-      res.end();
-    }
-  });
-
-  // setting a timeout on the subscription
-  client.timeout(sId, timeout, 0, () => {
-    if (res.headersSent) {
-      return;
-    }
-
-    clearTimeout(tId);
-    res.send("Queue timeout!");
-  });
-});
-
-app.get("/:queue/bloat/:length", (req, res) => {
-  res.setHeader("content-type", "text/plain");
-
-  // parsing params and headers
-  const queue = req.params.queue;
-  const length = Number(req.params.length);
-
-  // flagging a new queue to have messages of size X thousand zeroes
-  client.publish("queueBloating", JSON.stringify({
-    length: length,
-    queue: queue
-  }));
-
-  // setting up a full request timeout
-  const timeout = 5 * 1000;
-  const tId = setTimeout(() => {
-    if (res.headersSent) {
-      return;
-    }
-
-    res.send("Request timeout!");
-  }, timeout);
-
-  // starting up a subscriber waiting for a bloated message
-  const sId = client.subscribe(queue, (msg: string) => {
-    client.unsubscribe(sId);
-    clearTimeout(tId);
-
-    res.setHeader("content-encoding", "gzip");
-    res.write(Buffer.from(msg, "base64"));
-    res.end();
-  });
-
-  // setting a timeout on the subscription
-  client.timeout(sId, timeout, 0, () => {
-    if (res.headersSent) {
-      return;
-    }
-
-    clearTimeout(tId);
-    res.send("Queue timeout!");
-  });
-});
+// generating an app
+const app = getApp(client);
 
 // error handling
 client.on("error", (err: NATS.NatsError) => console.error(`${err.code}: ${err.message}`));
 
-// indicating activity
+// listening on app port
 app.listen(appPort, () => console.log(`Listening on ${appPort}`));
