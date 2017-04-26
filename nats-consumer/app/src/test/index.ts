79b279eba276cd8aec91bd707eaa3f5c0428482c
Adrian Parker <adrian.parker@smith.co>
2017-04-26 10:18:03 -0400

    Start testing the app routes.

---
+++ nats-consumer/app/src/test/index.ts
@@ -0,0 +1,23 @@
+import { test } from "ava";
+import * as supertest from "supertest";
+import * as HttpStatus from "http-status";
+import getApp from "../app";
+import getNatsClient from "../nats-client";
+
+const client = getNatsClient(process.env);
+const app = getApp(client);
+
+test("Timeout route should fail with 500", async () => {
+  return new Promise<void>((resolve, reject) => {
+    supertest(app)
+      .get("/timeout")
+      .expect(HttpStatus.INTERNAL_SERVER_ERROR)
+      .end((err: Error) => {
+        if (err) {
+          return reject(err);
+        }
+
+        resolve();
+      });
+  });
+});
