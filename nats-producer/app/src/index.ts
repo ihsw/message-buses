1b501cb839beb5e028ec998d8cad1ff2fb96bec9
Adrian Parker <adrian.parker@smith.co>
2017-05-01 12:46:10 -0400

    Creating sub-commands for nss-populate and nats-producer.

--- nats-producer/app/src/index.ts
+++ nats-producer/app/src/index.ts
@@ -1,38 +1,9 @@
 import * as process from "process";
-// import * as zlib from "zlib";
+import * as zlib from "zlib";
 import * as NATS from "nats";
-// import * as Stan from "node-nats-streaming";
+import * as program from "commander";
 import getNatsClient from "./nats-client";
 import NssClient from "./nss-client";
-
-// setting up nats queues
-// client.subscribe("queues", (msg) => client.publish(msg, "Pong"));
-
-// client.subscribe("queueWaiting", (msg) => {
-//   const req = JSON.parse(msg);
-//   const queue = req.queue;
-//   const count = Number(req.count);
-
-//   for (let i = 0; i < count; i++) {
-//     client.publish(queue, `Pong #${i}`);
-//   }
-// });
-
-// client.subscribe("queueBloating", (msg) => {
-//   const req = JSON.parse(msg);
-//   const queue = req.queue;
-//   const length = Number(req.length);
-
-//   const payload = "0".repeat(length * 1000);
-//   zlib.gzip(Buffer.from(payload), (err, buf) => {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-
-//     client.publish(queue, buf.toString("base64"));
-//   });
-// });
 
 const main = async () => {
   // connecting nats client
@@ -43,9 +14,58 @@ const main = async () => {
   const nssClient = new NssClient(natsClient, "ecp4", "ecp4");
   await nssClient.connect();
 
-  // closing out the connection
-  await nssClient.close();
-  nssClient.natsClient.close();
+  // program definition
+  program.version("0.0.1");
+
+  // populate action
+  program.command("nss-populate")
+    .description("Populates NSS with RFM catalogs")
+    .action(() => {
+      // filling up nss with rfm catalogs
+      console.log("Filling!");
+
+      // closing out the connection
+      nssClient.close().then(() => nssClient.natsClient.close());
+    });
+
+  // listening on queues for testing throughput
+  program.command("nats-producer")
+    .description("Listens on queues for testing throughput")
+    .action(() => {
+      console.log("Subscribing to queues");
+
+      // setting up nats queues
+      natsClient.subscribe("queues", (msg) => natsClient.publish(msg, "Pong"));
+
+      natsClient.subscribe("queueWaiting", (msg) => {
+        const req = JSON.parse(msg);
+        const queue = req.queue;
+        const count = Number(req.count);
+
+        for (let i = 0; i < count; i++) {
+          natsClient.publish(queue, `Pong #${i}`);
+        }
+      });
+
+      natsClient.subscribe("queueBloating", (msg) => {
+        const req = JSON.parse(msg);
+        const queue = req.queue;
+        const length = Number(req.length);
+
+        const payload = "0".repeat(length * 1000);
+        zlib.gzip(Buffer.from(payload), (err, buf) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          natsClient.publish(queue, buf.toString("base64"));
+        });
+      });
+    });
+
+  // parsing process args
+  program.parse(process.argv);
 };
 main()
   .then(() => process.exit(0))
