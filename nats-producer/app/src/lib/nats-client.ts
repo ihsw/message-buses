77c171a4ed428fec3d1405c36864aca157edb12c
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-05-08 12:53:04 -0400

    Fix spacing.

--- nats-producer/app/src/lib/nats-client.ts
+++ nats-producer/app/src/lib/nats-client.ts
@@ -1,14 +1,14 @@
 import * as NATS from "nats";
 
 export default (name: string, env: any): NATS.Client => {
-    // parsing env vars
-    const natsHost = env["NATS_HOST"];
-    const natsPort = Number(env["NATS_PORT"]);
+  // parsing env vars
+  const natsHost = env["NATS_HOST"];
+  const natsPort = Number(env["NATS_PORT"]);
 
-    // connecting
-    return NATS.connect(<NATS.ClientOpts>{
-        encoding: "binary",
-        name: name,
-        url: `nats://${natsHost}:${natsPort}`
-    });
+  // connecting
+  return NATS.connect(<NATS.ClientOpts>{
+    encoding: "binary",
+    name: name,
+    url: `nats://${natsHost}:${natsPort}`
+  });
 };
