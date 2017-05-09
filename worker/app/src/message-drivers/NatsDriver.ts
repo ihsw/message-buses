edd97760ab034f1bc586c4e6aa1d5030060c0032
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-05-09 07:51:27 -0400

    Continue filling out the nats-driver.

--- worker/app/src/message-drivers/NatsDriver.ts
+++ worker/app/src/message-drivers/NatsDriver.ts
@@ -1,19 +1,48 @@
 import * as NATS from "nats";
+import * as NSS from "node-nats-streaming";
 import { IMessageDriver, ISubscribeOptions } from "./IMessageDriver";
 
 export class NatsDriver implements IMessageDriver {
-  client: NATS.Client;
+  natsClient: NATS.Client;
+  nssClient: NSS.Stan;
 
-  constructor(client: NATS.Client) {
-    this.client = client;
+  constructor(natsClient: NATS.Client, nssClient: NSS.Stan) {
+    this.natsClient = natsClient;
+    this.nssClient = nssClient;
   }
 
   subscribe(opts: ISubscribeOptions) {
-    const sId = this.client.subscribe(opts.queue, opts.callback);
-    this.client.timeout(sId, opts.timeoutInMs, 0, () => opts.timeoutCallback);
+    const sId = this.natsClient.subscribe(opts.queue, opts.callback);
+    this.natsClient.timeout(sId, opts.timeoutInMs, 0, () => opts.timeoutCallback);
   }
 
   publish(queue: string, message: string | Buffer) {
-    this.client.publish(queue, message);
+    this.natsClient.publish(queue, message);
+  }
+
+  lastMessage(queue: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      const opts = this.nssClient.subscriptionOptions().setStartWithLastReceived();
+      const subscription = this.nssClient.subscribe(queue, `${queue}.workers`, opts);
+      const tId = setTimeout(() => reject(new Error("Subscription timeout!")), 2 * 1000);
+      subscription.on("message", (msg: NSS.Message) => {
+        clearTimeout(tId);
+        subscription.unsubscribe();
+
+        // resolving the message data
+        let result: string;
+        if (msg.getData() instanceof Buffer) {
+          result = msg.getData().toString();
+        } else {
+          result = <string>msg.getData();
+        }
+        
+        resolve(result);
+      });
+      subscription.on("error", (err) => {
+        clearTimeout(err);
+        reject(err);
+      });
+    });
   }
 }
