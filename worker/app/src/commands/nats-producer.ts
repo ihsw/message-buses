5dd2afd187785defdbc16a2c0cf4f3af96226ca1
Adrian Parker <ihsw.aparker@gmail.com>
2017-05-10 15:18:24 -0400

    Move producer-app code to its own lib file.
    Update nats-producer to take this in.

--- worker/app/src/commands/nats-producer.ts
+++ worker/app/src/commands/nats-producer.ts
@@ -1,8 +1,7 @@
-import * as zlib from "zlib";
-
 import { ConnectionInfo } from "./interfaces";
 import { GetDriver } from "../message-drivers/NatsDriver";
 import GetInflux from "../lib/influx";
+import run from "../lib/producer-app";
 import { defaultAppName } from "../lib/helper";
 
 export const ExpectedEnvVars: Array<string | ConnectionInfo> = [
@@ -14,36 +13,6 @@ export default async (env: any): Promise<void> => {
   const influx = await GetInflux(defaultAppName, env);
   const messageDriver = await GetDriver(influx, "nats-producer", "ecp4", env);
 
-  // setting up nats queues
-  messageDriver.subscribe({ queue: "queues", callback: (msg) => messageDriver.publish(msg, "Pong") });
-  messageDriver.subscribe({
-    queue: "queueWaiting",
-    callback: (msg) => {
-      const req = JSON.parse(msg);
-      const queue = req.queue;
-      const count = Number(req.count);
-
-      for (let i = 0; i < count; i++) {
-        messageDriver.publish(queue, `Pong #${i}`);
-      }
-    }
-  });
-  messageDriver.subscribe({
-    queue: "queueBloating",
-    callback: (msg) => {
-      const req = JSON.parse(msg);
-      const queue = req.queue;
-      const length = Number(req.length);
-
-      const payload = "0".repeat(length * 1000);
-      zlib.gzip(Buffer.from(payload), (err, buf) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-
-        messageDriver.publish(queue, buf.toString("base64"));
-      });
-    }
-  });
+  // running out the app
+  run(messageDriver);
 };
---
+++ worker/app/src/lib/producer-app.ts
@@ -0,0 +1,38 @@
+import * as zlib from "zlib";
+
+import { IMessageDriver } from "../message-drivers/IMessageDriver";
+
+export default (messageDriver: IMessageDriver) => {
+  // setting up queues
+  messageDriver.subscribe({ queue: "queues", callback: (msg) => messageDriver.publish(msg, "Pong") });
+  messageDriver.subscribe({
+    queue: "queueWaiting",
+    callback: (msg) => {
+      const req = JSON.parse(msg);
+      const queue = req.queue;
+      const count = Number(req.count);
+
+      for (let i = 0; i < count; i++) {
+        messageDriver.publish(queue, `Pong #${i}`);
+      }
+    }
+  });
+  messageDriver.subscribe({
+    queue: "queueBloating",
+    callback: (msg) => {
+      const req = JSON.parse(msg);
+      const queue = req.queue;
+      const length = Number(req.length);
+
+      const payload = "0".repeat(length * 1000);
+      zlib.gzip(Buffer.from(payload), (err, buf) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        messageDriver.publish(queue, buf.toString("base64"));
+      });
+    }
+  });
+};
