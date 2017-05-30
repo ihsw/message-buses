2c40636ac7e34e46771b01e17aa38acb0142971b
Adrian Parker <ihsw.aparker@gmail.com>
2017-05-30 12:33:59 -0400

    Add stop-instances command.

---
+++ vm-setup/stop-instances.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# misc
+SCP_IDENT="~/.ssh/google_compute_engine"
+DEFAULT_USER="gcloud-from-work"
+
+# fetching the list of ecp4 instances
+ECP4_INSTANCES=$(gcloud compute instances list --filter=ecp4 | tail -n +2 | awk '{print $1}')
+
+# going over each of them and stopping them
+for instance in $ECP4_INSTANCES
+do
+	gcloud compute stop $instance
+done
