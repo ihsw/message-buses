02f7c087c903944e03d4275606d4c3cf015ad238
Adrian Parker <ihsw.aparker@gmail.com>
2017-05-31 13:49:25 -0400

    Add start-nats-benchmarker command.

---
+++ vm-setup/start-nats-benchmarker.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# misc
+SCP_IDENT="~/.ssh/google_compute_engine"
+DEFAULT_USER="gcloud-from-work"
+GCLOUD_INSTANCE=$@
+
+# validation
+if [ -z $GCLOUD_INSTANCE ]; then
+    echo "GCLOUD_INSTANCE must be provided!"
+    exit 1
+fi
+
+GCLOUD_AUTH="$DEFAULT_USER@$GCLOUD_INSTANCE"
+
+# fetching the ip for the nats and metrics hosts
+NATS_HOST=$(gcloud compute instances list | grep nss-server-high-cpu | awk '{print $4}')
+METRICS_HOST=$(gcloud compute instances list | grep influxdb-server | awk '{print $4}')
+
+# building the image and running it
+gcloud compute ssh $GCLOUD_AUTH -- "cd ./message-buses && docker build -t ihsw/nats-benchmarker ./worker"
+gcloud compute ssh $GCLOUD_AUTH -- "docker run -it -e NATS_HOST=$NATS_HOST -e METRICS_HOST=$METRICS_HOST -e IS_CLUSTERING=1 -e APP_PORT=80 --entrypoint=\"\" -p 80:80 ihsw/nats-benchmarker ./bin/run-app nats-benchmarker -d 5m"
