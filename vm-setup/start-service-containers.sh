04ecb0cee310dde4b9905d8fe50f9b8f60b554da
Adrian Parker <ihsw.aparker@gmail.com>
2017-05-31 08:49:46 -0400

    Debugging.

--- vm-setup/start-service-containers.sh
+++ vm-setup/start-service-containers.sh
@@ -11,8 +11,10 @@ gcloud compute ssh $DEFAULT_USER@influxdb-server -- $'docker start $(docker ps -
 gcloud compute ssh $DEFAULT_USER@nss-server -- $'docker start $(docker ps -a | grep -i ihsw | awk \'{print $1}\')'
 
 # starting up the telegraf-collector on the work servers
-gcloud compute ssh $DEFAULT_USER@nats-consumer -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
-gcloud compute ssh $DEFAULT_USER@nats-consumer-high-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
+# gcloud compute ssh $DEFAULT_USER@nats-consumer -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
+# gcloud compute ssh $DEFAULT_USER@nats-consumer-high-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
+gcloud compute ssh $DEFAULT_USER@nats-consumer-very-high-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
 gcloud compute ssh $DEFAULT_USER@benchmarker -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
-gcloud compute ssh $DEFAULT_USER@nats-producer-high-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
-gcloud compute ssh $DEFAULT_USER@nats-producer-regular-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
+# gcloud compute ssh $DEFAULT_USER@nats-producer-regular-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
+# gcloud compute ssh $DEFAULT_USER@nats-producer-high-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
+gcloud compute ssh $DEFAULT_USER@nats-producer-very-high-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
