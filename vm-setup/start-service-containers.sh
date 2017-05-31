b1106a9a3ab4c187cbfcf074ebdf793fd44d084a
Adrian Parker <ihsw.aparker@gmail.com>
2017-05-31 13:40:16 -0400

    Update start-service-containers to more clearly convey intent.

--- vm-setup/start-service-containers.sh
+++ vm-setup/start-service-containers.sh
@@ -4,17 +4,16 @@
 SCP_IDENT="~/.ssh/google_compute_engine"
 DEFAULT_USER="gcloud-from-work"
 
-# starting up everything on the influxdb-server
-gcloud compute ssh $DEFAULT_USER@influxdb-server -- $'docker start $(docker ps -a | grep -i ihsw | awk \'{print $1}\')'
+# gathering up the instances with telegraf containers and starting telegraf on them
+TELEGRAF_INSTANCES=$(gcloud compute instances list | tail -n +2 | grep -vi terminated | grep -vi influxdb-server | awk '{print $1}')
+for instance in $TELEGRAF_INSTANCES
+do
+    gcloud compute ssh $DEFAULT_USER@$instance -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
+done
 
-# starting up everything on the nss-erver
-gcloud compute ssh $DEFAULT_USER@nss-server -- $'docker start $(docker ps -a | grep -i ihsw | awk \'{print $1}\')'
+# starting nats on the nats server
+NATS_INSTANCE=$(gcloud compute instances list | tail -n +2 | grep -vi terminated | grep -i nss-server | awk '{print $1}')
+gcloud compute ssh $DEFAULT_USER@$NATS_INSTANCE -- $'docker start $(docker ps -a | grep -i nats-server | awk \'{print $1}\')'
 
-# starting up the telegraf-collector on the work servers
-# gcloud compute ssh $DEFAULT_USER@nats-consumer -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
-# gcloud compute ssh $DEFAULT_USER@nats-consumer-high-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
-gcloud compute ssh $DEFAULT_USER@nats-consumer-very-high-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
-gcloud compute ssh $DEFAULT_USER@benchmarker -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
-# gcloud compute ssh $DEFAULT_USER@nats-producer-regular-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
-# gcloud compute ssh $DEFAULT_USER@nats-producer-high-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
-gcloud compute ssh $DEFAULT_USER@nats-producer-very-high-cpu -- $'docker start $(docker ps -a | grep -i telegraf-collector | awk \'{print $1}\')'
+# starting up everything on the influxdb-server except the telegraf container
+gcloud compute ssh $DEFAULT_USER@influxdb-server -- $'docker start $(docker ps -a | grep -i ihsw | grep -vi telegraf-collector | awk \'{print $1}\')'
