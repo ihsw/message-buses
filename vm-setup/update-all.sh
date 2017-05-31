c0258e250ec2351ca2b9dd9aaaf6057cdcd63571
Adrian Parker <ihsw.aparker@gmail.com>
2017-05-31 08:53:52 -0400

    Update update-all script to filter out offline instances.

--- vm-setup/update-all.sh
+++ vm-setup/update-all.sh
@@ -5,7 +5,7 @@ SCP_IDENT="~/.ssh/google_compute_engine"
 DEFAULT_USER="gcloud-from-work"
 
 # fetching the list of ecp4 instances
-ECP4_INSTANCES=$(gcloud compute instances list --filter=ecp4 | tail -n +2 | awk '{print $1}')
+ECP4_INSTANCES=$(gcloud compute instances list --filter=ecp4 | grep -vi terminated | tail -n +2 | awk '{print $1}')
 
 # going over each of them and updating them
 for instance in $ECP4_INSTANCES
