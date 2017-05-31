a3f669160f9e7650d65f7500cce5ad45068d1728
Adrian Parker <ihsw.aparker@gmail.com>
2017-05-31 12:58:24 -0400

    Fill out rudimentary benchmark code.

--- worker/app/src/lib/benchmarker.ts
+++ worker/app/src/lib/benchmarker.ts
@@ -3,25 +3,74 @@ import * as process from "process";
 import * as parseDuration from "parse-duration";
 
 import { ConnectionInfo } from "../commands/interfaces";
-import { IMessageDriver } from "../message-drivers/IMessageDriver";
+import { IMessageDriver, ISubscribeOptions } from "../message-drivers/IMessageDriver";
 import { MetricsCollector } from "../lib/MetricsCollector";
+import { getUniqueName } from "../lib/helper";
 
 export const ExpectedEnvVars: Array<string | ConnectionInfo> = [
   new ConnectionInfo("NATS_HOST", "NATS_PORT"),
   new ConnectionInfo("METRICS_HOST", "METRICS_PORT")
 ];
-export default async (messageDriver: IMessageDriver, metricsCollector: MetricsCollector, duration: string): Promise<void> => {
-  console.log(messageDriver.constructor.name);
-  console.log(metricsCollector.constructor.name);
-
+export default async (messageDriver: IMessageDriver, _: MetricsCollector, duration: string): Promise<void> => {
+  // parsing the duration
   const parsedDuration: number = parseDuration(duration);
+
+  // starting up an execution timer
   const startTime = process.hrtime();
-  return new Promise<void>((resolve) => {
-    setTimeout(() => {
+  const getExecutionTime = () => {
       const diff = process.hrtime(startTime);
-      const executionTimeInMs = Math.round((diff[0]*1e3 + diff[1]/1e6) * 10) / 10;
-      console.log(`Executed in ${executionTimeInMs}ms`);
+      return Math.round((diff[0]*1e3 + diff[1]/1e6) * 10) / 10;
+  };
+
+  // running it out
+  return new Promise<void>((resolve) => {
+    let running = true;
+
+    // setting up a timeout
+    const tId = setTimeout(() => { running = false }, parsedDuration);
+
+    // defining the exit
+    const exit = () => {
+      const executionTimeInMs = getExecutionTime();
+      console.log(`Executed in ${executionTimeInMs.toLocaleString()}ms`);
       resolve();
-    }, parsedDuration);
+    };
+
+    // handling unexpected exit
+    process.on("SIGINT", () => {
+      clearTimeout(tId);
+
+      running = false;
+      exit();
+    });
+    
+    // starting the loop up
+    console.log("Running benchmark");
+    const loop = () => {
+      const responseQueue = getUniqueName("hello-world");
+
+      const unsubscribe = messageDriver.subscribe(<ISubscribeOptions>{
+        queue: responseQueue,
+        parallel: true,
+        callback: () => {
+          if (!running) {
+            exit();
+
+            return;
+          }
+
+          loop();
+        },
+        timeoutInMs: 5*1000,
+        timeoutCallback: () => {
+          unsubscribe();
+
+          exit();
+        }
+      });
+
+      messageDriver.publish("queues", responseQueue);
+    };
+    loop();
   });
 };
