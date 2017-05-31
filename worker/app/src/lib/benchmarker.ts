3722d9ededc7eb871be653cad997ccca1dca574e
Adrian Parker <ihsw.aparker@gmail.com>
2017-05-31 12:32:04 -0400

    Update benchmarker to parse the duration and execute for that duration.

--- worker/app/src/lib/benchmarker.ts
+++ worker/app/src/lib/benchmarker.ts
@@ -1,3 +1,5 @@
+import * as process from "process";
+
 import * as parseDuration from "parse-duration";
 
 import { ConnectionInfo } from "../commands/interfaces";
@@ -8,10 +10,18 @@ export const ExpectedEnvVars: Array<string | ConnectionInfo> = [
   new ConnectionInfo("NATS_HOST", "NATS_PORT"),
   new ConnectionInfo("METRICS_HOST", "METRICS_PORT")
 ];
-export default (messageDriver: IMessageDriver, metricsCollector: MetricsCollector, duration: string) => {
-  console.log("Hello, world!");
+export default async (messageDriver: IMessageDriver, metricsCollector: MetricsCollector, duration: string): Promise<void> => {
   console.log(messageDriver.constructor.name);
   console.log(metricsCollector.constructor.name);
-  console.log(duration);
-  console.log(parseDuration(duration));
+
+  const parsedDuration: number = parseDuration(duration);
+  const startTime = process.hrtime();
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      const diff = process.hrtime(startTime);
+      const executionTimeInMs = Math.round((diff[0]*1e3 + diff[1]/1e6) * 10) / 10;
+      console.log(`Executed in ${executionTimeInMs}ms`);
+      resolve();
+    }, parsedDuration);
+  });
 };
