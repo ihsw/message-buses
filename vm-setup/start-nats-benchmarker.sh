d39d4d44f9341b31945925115b91373460f00eb5
Adrian Parker <ihsw.aparker@gmail.com>
2017-05-31 14:25:59 -0400

    Add git-pull to the start-benchmarker script.

--- vm-setup/start-nats-benchmarker.sh
+++ vm-setup/start-nats-benchmarker.sh
@@ -18,5 +18,5 @@ NATS_HOST=$(gcloud compute instances list | grep nss-server-high-cpu | awk '{pri
 METRICS_HOST=$(gcloud compute instances list | grep influxdb-server | awk '{print $4}')
 
 # building the image and running it
-gcloud compute ssh $GCLOUD_AUTH -- "cd ./message-buses && docker build -t ihsw/nats-benchmarker ./worker"
+gcloud compute ssh $GCLOUD_AUTH -- "cd ./message-buses && git pull && docker build -t ihsw/nats-benchmarker ./worker"
 gcloud compute ssh $GCLOUD_AUTH -- "docker run -it -e NATS_HOST=$NATS_HOST -e METRICS_HOST=$METRICS_HOST -e IS_CLUSTERING=1 -e APP_PORT=80 --entrypoint=\"\" -p 80:80 ihsw/nats-benchmarker ./bin/run-app nats-benchmarker -d 30s -w 30"
