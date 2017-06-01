f19ecc6308b477f278a2f39d6371fc0ee3723937
Adrian Parker <ihsw.aparker@gmail.com>
2017-06-01 09:50:27 -0400

    Bump up the expected response message size to benchmark load.

--- worker/app/src/lib/benchmarker.ts
+++ worker/app/src/lib/benchmarker.ts
@@ -8,7 +8,7 @@ import { MetricsCollector } from "../lib/MetricsCollector";
 import { getUniqueName } from "../lib/helper";
 
 export const waitingRequest = (messageDriver: IMessageDriver): Promise<void> => {
-  const expectedResponseMessages = 5;
+  const expectedResponseMessages = 20;
 
   return new Promise<void>((resolve, reject) => {
     // generating a unique response queue name
