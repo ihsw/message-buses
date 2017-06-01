3491beda3ce3112d59d01474f9ad665c7e957d9c
Adrian Parker <ihsw.aparker@gmail.com>
2017-06-01 13:54:38 -0400

    Add git-pull to the start-nats-consumer script.

--- vm-setup/start-nats-consumer.sh
+++ vm-setup/start-nats-consumer.sh
@@ -18,5 +18,5 @@ NATS_HOST=$(gcloud compute instances list | grep nss-server-high-cpu | awk '{pri
 METRICS_HOST=$(gcloud compute instances list | grep influxdb-server | awk '{print $4}')
 
 # building the image and running it
-gcloud compute ssh $GCLOUD_AUTH -- "cd ./message-buses && docker build -t ihsw/nats-consumer ./worker"
+gcloud compute ssh $GCLOUD_AUTH -- "cd ./message-buses && git pull && docker build -t ihsw/nats-consumer ./worker"
 gcloud compute ssh $GCLOUD_AUTH -- "docker run -it -e NATS_HOST=$NATS_HOST -e METRICS_HOST=$METRICS_HOST -e IS_CLUSTERING=1 -e APP_PORT=80 -p 80:80 ihsw/nats-consumer ./bin/run-app nats-consumer"
