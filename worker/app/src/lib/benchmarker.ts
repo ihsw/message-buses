7422af2e2449effa0a1608ffda1fa182b0ab2fa3
Adrian Parker <ihsw.aparker@gmail.com>
2017-06-05 09:54:05 -0400

    Bump up expected response messages, for benchmarking.

--- worker/app/src/lib/benchmarker.ts
+++ worker/app/src/lib/benchmarker.ts
@@ -8,7 +8,7 @@ import { MetricsCollector } from "../lib/MetricsCollector";
 import { getUniqueName } from "../lib/helper";
 
 export const waitingRequest = (messageDriver: IMessageDriver): Promise<void> => {
-  const expectedResponseMessages = 100;
+  const expectedResponseMessages = 1000;
 
   return new Promise<void>((resolve, reject) => {
     // generating a unique response queue name
