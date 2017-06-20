d0038e90d4d7beb227230d1dded6e639f618cc7f
Adrian Parker <ihsw.aparker@gmail.com>
2017-06-20 13:43:28 -0400

    Reduce benchmarking workload for testing rabbit.

--- worker/app/src/lib/benchmarker.ts
+++ worker/app/src/lib/benchmarker.ts
@@ -7,7 +7,7 @@ import { MetricsCollector } from "../lib/MetricsCollector";
 import { getUniqueName } from "../lib/helper";
 
 export const waitingRequest = (messageDriver: IMessageDriver): Promise<void> => {
-  const expectedResponseMessages = 300;
+  const expectedResponseMessages = 10;
 
   return new Promise<void>((resolve, reject) => {
     // generating a unique response queue name
