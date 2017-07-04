d1405546cab4285afd317b7931f100c318529b2d
Adrian Parker <ihsw.aparker@gmail.com>
2017-07-04 11:00:57 -0400

    Fix typoes.

--- vm-setup/start-nats-benchmarker.sh
+++ vm-setup/start-nats-benchmarker.sh
@@ -14,9 +14,9 @@ fi
 GCLOUD_AUTH="$DEFAULT_USER@$GCLOUD_INSTANCE"
 
 # fetching the ip for the nats and metrics hosts
-NATS_HOST=$(gcloud compute instances list | grep nss-server-high-cpu | awk '{print $4}')
+NATS_HOST=$(gcloud compute instances list | grep message-bus | awk '{print $4}')
 METRICS_HOST=$(gcloud compute instances list | grep influxdb-server | awk '{print $4}')
 
 # building the image and running it
 gcloud compute ssh $GCLOUD_AUTH -- "cd ./message-buses && git pull && docker build -t ihsw/nats-benchmarker ./worker"
-gcloud compute ssh $GCLOUD_AUTH -- "docker run -it -e NATS_HOST=$NATS_HOST -e METRICS_HOST=$METRICS_HOST -e IS_CLUSTERING=1 -e DRIVER_TYPE=rabbit ihsw/nats-benchmarker ./bin/run-app nats-benchmarker -d 2m -w 200"
+gcloud compute ssh $GCLOUD_AUTH -- "docker run -it -e NATS_HOST=$NATS_HOST -e METRICS_HOST=$METRICS_HOST -e IS_CLUSTERING=1 -e DRIVER_TYPE=nats ihsw/nats-benchmarker ./bin/run-app nats-benchmarker -d 2m -w 1"
