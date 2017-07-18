d8f95c122f10e271d4666a6908b4b15d95fadacb
Adrian Parker <ihsw.aparker@gmail.com>
2017-07-18 09:56:31 -0400

    Ramp down duration to accommodate accurate rabbit benchmarking.

--- vm-setup/start-rabbit-benchmarker.sh
+++ vm-setup/start-rabbit-benchmarker.sh
@@ -14,9 +14,9 @@ fi
 GCLOUD_AUTH="$DEFAULT_USER@$GCLOUD_INSTANCE"
 
 # fetching the ip for the nats and metrics hosts
-RABBIT_HOST=$(gcloud compute instances list | grep message-bus-4cpu | awk '{print $4}')
+RABBIT_HOST=$(gcloud compute instances list | grep message-bus | awk '{print $4}')
 METRICS_HOST=$(gcloud compute instances list | grep influxdb-server | awk '{print $4}')
 
 # building the image and running it
 gcloud compute ssh $GCLOUD_AUTH -- "cd ./message-buses && git pull && docker build -t ihsw/rabbit-benchmarker ./worker"
-gcloud compute ssh $GCLOUD_AUTH -- "docker run -it -e RABBIT_HOST=$RABBIT_HOST -e METRICS_HOST=$METRICS_HOST -e IS_CLUSTERING=1 -e DRIVER_TYPE=rabbit ihsw/rabbit-benchmarker ./bin/run-app rabbit-benchmarker -d 2m -w 1"
+gcloud compute ssh $GCLOUD_AUTH -- "docker run -it -e RABBIT_HOST=$RABBIT_HOST -e METRICS_HOST=$METRICS_HOST -e IS_CLUSTERING=1 -e DRIVER_TYPE=rabbit ihsw/rabbit-benchmarker ./bin/run-app rabbit-benchmarker -d 1m -w 1"
