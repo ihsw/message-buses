13297b756057f347a79a1f91a2576818cfdae752
Adrian Parker <ihsw.aparker@gmail.com>
2017-07-19 10:23:21 -0400

    Add test and testdata for the rabbit fetcher.

--- nats-monitor/app/fetcher/rabbit_test.go
+++ nats-monitor/app/fetcher/rabbit_test.go
@@ -1,6 +1,25 @@
 package fetcher
 
 import "testing"
+import "io/ioutil"
+import "github.com/stretchr/testify/assert"
 
-func TestRabbitGet(t *testing.T) {
+func TestRabbitRead(t *testing.T) {
+	contents, err := ioutil.ReadFile("./testdata/rabbit.json")
+	if err != nil {
+		t.Fatal(err)
+
+		return
+	}
+
+	r := rabbit{}
+	data, err := r.read(contents)
+	if err != nil {
+		t.Fatal(err)
+
+		return
+	}
+
+	assert.NotZero(t, data.InMsgs)
+	assert.NotZero(t, data.OutMsgs)
 }
